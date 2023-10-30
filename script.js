55a481cf3eda233b992a7baddbd1eb17bd8df508
MeFinity <contact@mefinity.eu.org>
2023-10-30 12:05:41 +0700

    make github happy

--- script.js
+++ script.js
@@ -1,68 +1,75 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-const imageFolder = 'emoji';
-const imageGrid = document.getElementById('image-grid');
-const searchInput = document.getElementById('search-bar');
+    const imageGrid = document.getElementById('image-grid');
+    const searchInput = document.getElementById('search-bar');
 
-function loadImages() {
-    fetchImages(imageFolder).then(images => {
-        images.forEach(image => {
-            const imageName = decodeURI(image.split('/').pop().split('.')[0]);
-            const imageContainer = document.createElement('div');
-            imageContainer.className = 'image-container';
+    function loadImagesFromZip(zipFile) {
+        JSZip.loadAsync(zipFile)
+            .then(function (zip) {
+                const imagePromises = [];
 
-            const img = document.createElement('img');
-            img.src = image;
-            img.alt = imageName;
+                zip.forEach(function (relativePath, file) {
+                    if (file.dir) return;
 
-            const imageCaption = document.createElement('div');
-            imageCaption.className = 'image-name';
-            imageCaption.innerText = imageName;
+                    const imageName = file.name.split('/').pop().split('.')[0];
+                    const imageContainer = document.createElement('div');
+                    imageContainer.className = 'image-container';
 
-            imageContainer.appendChild(img);
-            imageContainer.appendChild(imageCaption);
+                    imagePromises.push(
+                        file.async('blob').then(function (blob) {
+                            const imageUrl = URL.createObjectURL(blob);
 
-            imageGrid.appendChild(imageContainer);
+                            const img = document.createElement('img');
+                            img.src = imageUrl;
+                            img.alt = imageName;
 
-            imageContainer.addEventListener('click', () => {
-                downloadImage(image, imageName);
+                            const imageCaption = document.createElement('div');
+                            imageCaption.className = 'image-name';
+                            imageCaption.innerText = imageName;
+
+                            imageContainer.appendChild(img);
+                            imageContainer.appendChild(imageCaption);
+
+                            imageGrid.appendChild(imageContainer);
+
+                            imageContainer.addEventListener('click', () => {
+                                downloadImage(imageUrl, imageName);
+                            });
+                        })
+                    );
+                });
+
+                return Promise.all(imagePromises);
             });
+    }
+
+    function handleSearch() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const imageContainers = document.querySelectorAll('.image-container');
+
+        imageContainers.forEach(container => {
+            const imageName = container.querySelector('.image-name').innerText.toLowerCase();
+            if (imageName.includes(searchTerm)) {
+                container.style.display = 'block';
+            } else {
+                container.style.display = 'none';
+            }
         });
-    });
-}
+    }
 
-async function fetchImages(folder) {
-    const response = await fetch(folder);
-    const text = await response.text();
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(text, 'text/html');
-    const imageLinks = Array.from(htmlDocument.links).map(link => link.href);
-    return imageLinks.filter(link => /\.(jpe?g|png|gif)$/i.test(link));
-}
+    function downloadImage(url, fileName) {
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
 
-function handleSearch() {
-    const searchTerm = searchInput.value.toLowerCase();
-    const imageContainers = document.querySelectorAll('.image-container');
+    searchInput.addEventListener('input', handleSearch);
 
-    imageContainers.forEach(container => {
-        const imageName = container.querySelector('.image-name').innerText.toLowerCase();
-        if (imageName.includes(searchTerm)) {
-            container.style.display = 'block';
-        } else {
-            container.style.display = 'none';
-        }
-    });
-}
-
-function downloadImage(url, fileName) {
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = fileName;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
-
-searchInput.addEventListener('input', handleSearch);
-
-loadImages();
-})
+    fetch('emoji.zip')
+        .then(response => response.arrayBuffer())
+        .then(data => {
+            loadImagesFromZip(data);
+        });
+});
